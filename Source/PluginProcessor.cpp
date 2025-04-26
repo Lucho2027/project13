09f832e49eaa35b88b81d0a62b1681018e769987
lucho2027 <luis01.ro@gmail.com>
2025-04-26 10:20:39 -0500

    Added TODO's

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -143,6 +143,16 @@ void Project13AudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, ju
     // this code if your algorithm always overwrites all the output channels.
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
+    
+    //TODO: add APVTS
+    //TODO: create audio parameters for all dsp choices
+    //TODO: update DSP here from audio parameters
+    //TODO: save/load settings
+    //TODO: save/load DSP order
+    //TODO: Drag-To-Reorder GUI
+    //TODO: GUI desing for each DSP instance?
+    //TODO: metering
+    //TODO: prepare all DSP
 
     auto newDSPOrder = DSP_Order();
 
